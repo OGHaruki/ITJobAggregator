94c55c1590f8a604ba4590c16d2c20f7feaed160
Adam Jakubowski <66510475+OGHaruki@users.noreply.github.com>
2025-02-14 13:27:01 +0100

    Delete clear_database.sql

--- clear_database.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE TABLE job_offer_location;
-TRUNCATE TABLE job_offer_required_skills;
-TRUNCATE TABLE job_location CASCADE;
-TRUNCATE TABLE required_skills CASCADE;
-TRUNCATE TABLE job_offer CASCADE;
